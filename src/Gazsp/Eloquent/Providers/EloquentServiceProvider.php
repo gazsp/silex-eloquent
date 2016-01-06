e3cf307757d1c370908b965cfad751fa54856695
Gary Pearman <gpearman@gmail.com>
2016-01-06 00:12:21 +0000

    Renamed

---
+++ src/Gazsp/Eloquent/Providers/EloquentServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Gazsp\Eloquent\Providers;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class EloquentServiceProvider implements ServiceProviderInterface {
+
+    public function register(Application $app) {
+        $this['db'] = $this->share(function() use($app) {
+            $capsule = new Capsule;
+            $capsule->addConnection(
+                $app['config']['database']['connections'][
+                    $app['config']['database']['connection']
+                ]
+            );
+
+            return $capsule;
+        });
+
+        $this['db']->setAsGlobal();
+        $this['db']->bootEloquent();
+    }
+
+    public function boot(Application $app) {
+    }
+
+}
